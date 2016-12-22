c02a8cc974d87e88d2096b35ba2079aafac440c9
Tim Ho <wentian.he@qq.com>
2016-12-22 15:24:29 +0800

    add basic

---
+++ src/python27/basic/input.py
@@ -0,0 +1,11 @@
+print "hello, world"
+print "The quick brown fox", "jumps over", "the lazy dog"
+print 300
+print 100 + 200
+print "100 + 200 =", 100 + 200
+
+name = raw_input("enter your name:")
+print "hello", name
+
+
+
